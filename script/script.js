26ca7d060313f2ba86a5a3987ceedf2a85acd875
alifiarzql <rizqulalifia@gmail.com>
2025-02-12 14:33:07 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
+const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 
+const getRandomWord = () => {
+    // Memilih kata dan petunjuk secara acak dari wordList
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+// Membuat tombol keyboard
 for (let i = 97; i <= 122; i++) {
-    console button =  document.createElement("button");
-    console.log(String.fromCharCode(i));
+    let button = document.createElement("button");
+    button.innerText = String.fromCharCode(i); // Menampilkan huruf
+    button.classList.add("key-button"); // Opsional untuk styling
     keyboardDiv.appendChild(button);
 }
