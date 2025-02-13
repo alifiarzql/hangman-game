d6696d59775b9e7b9b251c19858a9fcf259567ba
alifiarzql <rizqulalifia@gmail.com>
2025-02-13 15:37:57 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -1,12 +1,31 @@
 const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 
+let currentWord;
+
 const getRandomWord = () => {
     // Memilih kata dan petunjuk secara acak dari wordList
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word, hint);
+    currentWord = word;
+    console.log(word);
     document.querySelector(".hint-text b").innerText = hint;
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join(""); 
+}
+
+const initGame = (button, clickedLetter) => {
+    //checking if clickedLetter is exist on the currentWord
+    if(currentWord.includes(clickedLetter)) {
+        //showing all correct
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add(guessed);
+            }
+        })
+    } else {
+        console.log(clickedLetter, " is not exist on the word");
+    }
+    
 }
 
 // Membuat tombol keyboard
@@ -14,6 +33,7 @@ for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i); 
     keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
 }
 
 getRandomWord();
