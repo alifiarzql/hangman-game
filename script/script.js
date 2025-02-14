6442b131dc0d1e0df8e077201d93c0f967ad1d79
alifiarzql <rizqulalifia@gmail.com>
2025-02-14 22:50:44 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -6,37 +6,42 @@ let currentWord, wrongGuessCount = 0;
 const maxGuesses = 6;
 
 const getRandomWord = () => {
-    // Memilih kata dan petunjuk secara acak dari wordList
-    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    currentWord = word;
-    console.log(word);
-    document.querySelector(".hint-text b").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join(""); 
-}
+  // Memilih kata dan petunjuk secara acak dari wordList
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word;
+  console.log(word);
+  document.querySelector(".hint-text b").innerText = hint;
+  wordDisplay.innerHTML = word
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+};
 
 const initGame = (button, clickedLetter) => {
-    //checking if clickedLetter is exist on the currentWord
-    if(currentWord.includes(clickedLetter)) {
-        //showing all correct
-        [...currentWord].forEach((letter, index) => {
-            if(letter === clickedLetter) {
-                wordDisplay.querySelectorAll("li")[index].innerText = letter;
-                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
-            }
-        })
-    } else {
-        wrongGuessCount++;
-    }
-    
+  //checking if clickedLetter is exist on the currentWord
+  if (currentWord.includes(clickedLetter)) {
+    //showing all correct
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    });
+  } else {
+    wrongGuessCount++;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+  }
 }
 guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
 // Membuat tombol keyboard
 for (let i = 97; i <= 122; i++) {
-    const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i); 
-    keyboardDiv.appendChild(button);
-    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i))
+  );
 }
 
 getRandomWord();
